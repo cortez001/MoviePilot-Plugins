5a131c4aaf26cfa005407f7990068c5fd297235d
cortez001 <87006718+cortez001@users.noreply.github.com>
2026-01-06 23:04:22 +0800

    Update __init__.py

--- plugins.v2/speedlimiter/__init__.py
+++ plugins.v2/speedlimiter/__init__.py
@@ -20,7 +20,7 @@ class SpeedLimiter(_PluginBase):
     # 插件图标
     plugin_icon = "Librespeed_A.png"
     # 插件版本
-    plugin_version = "2.1"
+    plugin_version = "2.2"
     # 插件作者
     plugin_author = "Shurelol"
     # 作者主页
