143eed818a5859377e7da62a36f088010f05ba89
cortez001 <87006718+cortez001@users.noreply.github.com>
2026-01-06 23:06:59 +0800

    Update __init__.py

--- plugins.v2/speedlimiter/__init__.py
+++ plugins.v2/speedlimiter/__init__.py
@@ -26,7 +26,7 @@ class SpeedLimiter(_PluginBase):
     # 作者主页
     author_url = "https://github.com/Shurelol"
     # 插件配置项ID前缀
-    plugin_config_prefix = "speedlimit_"
+    plugin_config_prefix = "speedlimitv2_"
     # 加载顺序
     plugin_order = 11
     # 可使用的用户级别
